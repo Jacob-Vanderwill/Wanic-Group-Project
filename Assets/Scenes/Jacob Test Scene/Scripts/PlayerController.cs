c38c2529db290e935e29e17430d1ca4fb2a36936
Jacob-Vanderwill <jacobvanderwill@gmail.com>
2025-03-10 15:01:45 -0700

    Added BASIC player controller

--- Assets/Scenes/Jacob Test Scene/Scripts/PlayerController.cs
+++ Assets/Scenes/Jacob Test Scene/Scripts/PlayerController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
+    public float Speed;
+    public float SpeedSprint;
+    public float SinkSpeed;
+
+    private Vector3 inputMovement = Vector3.zero;
+    private bool isSprinting;
+
+    private Rigidbody2D myRB;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        inputMovement.x = Input.GetAxisRaw("Horizontal");
+        inputMovement.y = Input.GetAxisRaw("Vertical");
+        isSprinting = Input.GetKeyDown(KeyCode.LeftShift);
+    }
+    private void FixedUpdate()
+    {
+        if (isSprinting)
+        {
+            myRB.AddForce(inputMovement.normalized * SpeedSprint);
+            Debug.Log("Sprinting");
+        }
+        else
+        {
+            myRB.AddForce(inputMovement.normalized * Speed);
+        }
     }
 }
