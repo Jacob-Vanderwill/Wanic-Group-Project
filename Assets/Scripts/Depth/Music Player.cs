5d78ecc1fc3a7445ff1154695cdab2c1bcf5bcc0
squidoctopus360 <158018558+squidoctopus360@users.noreply.github.com>
2025-04-01 13:57:50 -0700

    music stuff

---
+++ Assets/Scripts/Depth/Music Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource[] musicPlayer;
+    public AudioClip BaseMusicTrack;
+    public AudioClip FirstDepthAddon;
+    public AudioClip SecondDepthAddon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        musicPlayer = GetComponents<AudioSource>();
+        musicPlayer[0].volume = 1;
+        musicPlayer[1].volume = 0;
+        musicPlayer[2].volume = 0;
+        musicPlayer[0].clip = BaseMusicTrack;
+        musicPlayer[1].clip = FirstDepthAddon;
+        musicPlayer[2].clip = SecondDepthAddon;
+        musicPlayer[0].Play();
+        musicPlayer[1].Play();
+        musicPlayer[2].Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x >= 300)
+        {  
+            musicPlayer[1].volume += Time.deltaTime;
+        }
+        if(transform.position.x >= 600)
+        {
+            musicPlayer[2].volume += Time.deltaTime/3;
+        }
+    }
+}
