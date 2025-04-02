57f746d530b6f28ccfd8f7a8f1baf9dd49c7e454
squidoctopus360 <158018558+squidoctopus360@users.noreply.github.com>
2025-04-02 14:56:58 -0700

    grfewefw

--- Assets/Scripts/Play Sound.cs
+++ Assets/Scripts/Play Sound.cs
@@ -4,38 +4,10 @@ using UnityEngine;
 
 public class PlaySound : MonoBehaviour
 {
-    private List<AudioSource> NewAudioSources;
-    private List<AudioSource> audiosources;
+    public AudioSource AudioPlayer;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        audiosources.Clear();
-        foreach(AudioSource source in NewAudioSources)
-        {
-            Destroy(source);
-            if (!source.isPlaying)
-            {
-                audiosources.Add(source);
-            }
-            
-            
-        }
-        foreach(AudioSource source in audiosources)
-        {
-            NewAudioSources.Remove(source);
-        }
-    }
     public void SoundPlay(AudioClip AC)
     {
-        AudioSource NewAudioSource = this.gameObject.AddComponent<AudioSource>();
-        NewAudioSource.clip = AC;
-        NewAudioSource.Play();
-        NewAudioSources.Add(NewAudioSource);
+        AudioPlayer.PlayOneShot(AC);
     }
 }
